c8ed730a66e90462a966c4dc6c1c9ccd5a5a4422
Romario117 <rfls_93@hotmail.com>
2025-03-21 20:49:14 -0600

    Add initial C++ source files and VS Code configuration

---
+++ datatype/types.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    int age = 30;
+    float weight = 65.5;
+    char initial = 'A';
+    bool isHappy = true;
+
+    std::cout << "Age: " << age << std::endl;
+    std::cout << "Weight: " << weight << std::endl;
+    std::cout << "Initial: " << initial << std::endl;
+    std::cout << "Is happy: " << isHappy << std::endl;
+
+    return 0;
+}
